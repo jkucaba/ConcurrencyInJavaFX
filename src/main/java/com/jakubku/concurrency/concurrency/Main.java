c07d2ac2fc53a04e59e6636ef9885fad54f73b99
Jakub <jakub.kucaba@gmail.com>
2023-10-21 13:40:37 +0200

    The Concurrency Problem Demo

--- src/main/java/com/jakubku/concurrency/concurrency/Main.java
+++ src/main/java/com/jakubku/concurrency/concurrency/Main.java
@@ -1,12 +1,33 @@
 package com.jakubku.concurrency.concurrency;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        Button stopUI = new Button("Stop UI");
+        Button exit = new Button("Exit");
+        stopUI.setOnAction(actionEvent -> {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        exit.setOnAction(actionEvent -> Platform.exit());
+        root.getChildren().addAll(stopUI, exit);
+        Scene scene = new Scene(root, 500, 450);
+        stage.setScene(scene);
+        stage.setTitle("Concurrency");
+        stage.show();
     }
 
     public static void main(String[] args) {
