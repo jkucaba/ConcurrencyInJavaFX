924651e8c373c5aac7d29d7782d6f0bbf6d5f6c5
Jakub <jakub.kucaba@gmail.com>
2023-10-22 14:14:34 +0200

    Adding Service

--- src/main/java/com/jakubku/concurrency/concurrency/Main.java
+++ src/main/java/com/jakubku/concurrency/concurrency/Main.java
@@ -1,6 +1,9 @@
 package com.jakubku.concurrency.concurrency;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -10,23 +13,34 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     Button startBtn = new Button("Start");
+    Button resetBtn = new Button("Reset");
     Button cancelBtn = new Button("Cancel");
     Button exitBtn = new Button("Exit");
-
-    EvenNumTask task = new EvenNumTask(1, 20, 1000);
+    boolean onceStarted = false;
+    Service<ObservableList<Integer>> service = new Service<>(){
+        @Override
+        protected Task<ObservableList<Integer>> createTask() {
+            return new EvenNumTask(1, 20, 1000);
+        }
+    };
     @Override
     public void start(Stage stage) {
         startBtn.setOnAction(
                 e ->{
-                    Thread bgThread = new Thread(task);
-                    bgThread.setDaemon(true);
-                    bgThread.start();
+                    if(onceStarted){
+                        service.restart();
+                    } else {
+                        service.start();
+                        onceStarted = true;
+                        startBtn.setText("Restart");
+                    }
                 });
-        cancelBtn.setOnAction(e -> task.cancel());
+        cancelBtn.setOnAction(e -> service.cancel());
+        resetBtn.setOnAction(e -> service.reset());
         exitBtn.setOnAction(e -> stage.close());
 
-        GridPane pane = new WorkerUI(task);
-        HBox box = new HBox(5, startBtn, cancelBtn, exitBtn);
+        GridPane pane = new WorkerUI(service);
+        HBox box = new HBox(5, startBtn, resetBtn, cancelBtn, exitBtn);
 
         BorderPane root = new BorderPane();
         root.setCenter(pane);
