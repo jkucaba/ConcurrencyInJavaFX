0e70b9be3573e84137b9a3229b61b9094c31c149
Jakub <jakub.kucaba@gmail.com>
2023-10-22 13:28:18 +0200

    Adding the WorkerUI

--- src/main/java/com/jakubku/concurrency/concurrency/Main.java
+++ src/main/java/com/jakubku/concurrency/concurrency/Main.java
@@ -11,26 +11,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox(10);
-        root.setAlignment(Pos.CENTER);
-        Button stopUI = new Button("Stop UI");
-        Button exit = new Button("Exit");
-        stopUI.setOnAction(actionEvent -> {
-            try {
-                Thread.sleep(10000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        exit.setOnAction(actionEvent -> Platform.exit());
-        root.getChildren().addAll(stopUI, exit);
-        Scene scene = new Scene(root, 500, 450);
-        stage.setScene(scene);
-        stage.setTitle("Concurrency");
-        stage.show();
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
